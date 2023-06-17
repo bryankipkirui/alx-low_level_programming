f50dade3c48ba4e93de2f3f774fd8d75e05b8ba1
bryankipkirui <bryanlogan750@gmail.com>
2023-06-16 22:13:02 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 0; b < 8; a++)
+	for (a = 0; a < 8; a++)
 	{
 	for (b = a + 1; b < 9; b++)
 	{
@@ -17,12 +17,12 @@ int main(void)
 		putchar('0' + a);
 		putchar('0' + b);
 		putchar('0' + c);
-		if (a < 8 || b < 9 || c < 10)
+		if (a < 7 || b < 8 || c < 9)
+
 		{
 		putchar(',');
 		putchar(' ');
 		}
-
 	}
 	}
 	}
