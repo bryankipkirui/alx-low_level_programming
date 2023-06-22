584f2ad80c412b838fdcd9f9b09615764e403616
bryankipkirui <bryanlogan750@gmail.com>
2023-06-22 16:11:51 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
+ * _putchar - writes the character i to stdout
  * print_most_numbers - print numbers from 0 to 9 excluding 2 and 4
- * Return: void
+ * Return: always 0
  */
 
 void print_most_numbers(void)
 {
-	int i
+	int i;
 
 	for (i = 48; i <= 57; i++)
 	{
 		if (i == 50 || i == 52)
-		{
 		continue;
-		}
-		else
-		{
+
 		_putchar(i);
-		}
+
 	}
 	_putchar('\n');
 }
